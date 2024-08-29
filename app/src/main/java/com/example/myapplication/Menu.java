8c608c1338758ca35ad23406886cdabe5637690b
Sergio Lopez <salopeza@udistrital.edu.co>
2024-08-29 18:46:23 -0500

    ID btn_menu completo, función btn listo

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -19,8 +19,84 @@ public class Menu extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
 
-        Button btnInicio = findViewById(R.id.btn_HidroTrack);
-        btnInicio.setOnClickListener(new View.OnClickListener() {
+        //declaración los btn
+        Button btnHidroTrack = findViewById(R.id.btn_HidroTrack);
+        Button btnEcoEnergy = findViewById(R.id.btn_EcoEnergy);
+        Button btnRepaso = findViewById(R.id.btn_Repaso);
+        Button btnMargenOpti = findViewById(R.id.btn_MargenOpti);
+        Button btnReproductor = findViewById(R.id.btn_Reproductor);
+        Button btnDiseno = findViewById(R.id.btn_Diseno);
+        Button btnScroll = findViewById(R.id.btn_Scroll);
+        Button btnTablas = findViewById(R.id.btn_Tablas);
+        Button btnRepFinal = findViewById(R.id.btn_RepFinal);
+
+        //Funcionalidad del btn
+        //hidro
+        btnHidroTrack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //eco
+        btnEcoEnergy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //rep
+        btnRepaso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //margen
+        btnMargenOpti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //repo
+        btnReproductor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //dise
+        btnDiseno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //scroll
+        btnScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //tablas
+        btnTablas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intento = new Intent(Menu.this, Design.class);
+                startActivity(intento);
+            }
+        });
+        //repfin
+        btnRepFinal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intento = new Intent(Menu.this, Design.class);
